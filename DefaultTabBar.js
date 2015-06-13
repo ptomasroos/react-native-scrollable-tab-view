c111d7746a87b7cebe07e90e3d73eede4970df53
Brent Vatne <brentvatne@gmail.com>
2015-06-12 17:45:16 -0700

    Injectable tab bar component

---
+++ DefaultTabBar.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} = React;
+
+var deviceWidth = require('Dimensions').get('window').width;
+var precomputeStyle = require('precomputeStyle');
+var TAB_UNDERLINE_REF = 'TAB_UNDERLINE';
+
+var styles = StyleSheet.create({
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 10,
+  },
+
+  tabs: {
+    height: 50,
+    flexDirection: 'row',
+    marginTop: 20,
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomColor: '#ccc',
+  },
+});
+
+var DefaultTabBar = React.createClass({
+  propTypes: {
+    goToPage: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    tabs: React.PropTypes.array
+  },
+
+  renderTabOption(name, page) {
+    var isTabActive = this.props.activeTab === page;
+
+    return (
+      <TouchableOpacity key={name} onPress={() => this.props.goToPage(page)}>
+        <View style={[styles.tab]}>
+          <Text style={{color: isTabActive ? 'navy' : 'black', fontWeight: isTabActive ? 'bold' : 'normal'}}>{name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  },
+
+  setAnimationValue(value) {
+    this.refs[TAB_UNDERLINE_REF].setNativeProps(precomputeStyle({
+      left: (deviceWidth * value) / this.props.tabs.length
+    }));
+  },
+
+  render() {
+    var numberOfTabs = this.props.tabs.length;
+    var tabUnderlineStyle = {
+      position: 'absolute',
+      width: deviceWidth / numberOfTabs,
+      height: 4,
+      backgroundColor: 'navy',
+      bottom: 0,
+    };
+
+    return (
+      <View style={styles.tabs}>
+        {this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
+        <View style={tabUnderlineStyle} ref={TAB_UNDERLINE_REF} />
+      </View>
+    );
+  },
+});
+
+module.exports = DefaultTabBar;
--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 var React = require('react-native');
 var {
-  StyleSheet,
   Text,
   View,
   ScrollView,
@@ -10,18 +9,22 @@ var {
   PanResponder,
 } = React;
 
+var DefaultTabBar = require('./DefaultTabBar');
 var deviceWidth = require('Dimensions').get('window').width;
 var rebound = require('rebound');
 var precomputeStyle = require('precomputeStyle');
+var TAB_BAR_REF = 'TAB_BAR';
 
 var ScrollableTabView = React.createClass({
+  getInitialState() {
+    return { currentPage: 0 };
+  },
+
   componentDidMount() {
     this._scrollSpring.setCurrentValue(0);
   },
 
   componentWillMount() {
-    this._currentPage = 0;
-
     // Initialize the spring that will drive animations
     this.springSystem = new rebound.SpringSystem();
     this._scrollSpring = this.springSystem.createSpring();
@@ -29,9 +32,8 @@ var ScrollableTabView = React.createClass({
 
     this._scrollSpring.addListener({
       onSpringUpdate: () => {
-        if (!this.scrollView || !this.tabUnderline) { return }
+        if (!this.scrollView) { return; }
 
-        var numberOfTabs = this.props.children.length;
         var currentValue = this._scrollSpring.getCurrentValue();
         var offsetX = deviceWidth * currentValue;
 
@@ -39,9 +41,8 @@ var ScrollableTabView = React.createClass({
           transform: [{translateX: -1 * offsetX}],
         }));
 
-        this.tabUnderline.setNativeProps(precomputeStyle({
-          left: offsetX / numberOfTabs
-        }));
+        // Pass the currentValue on to the tabBar component
+        this.refs[TAB_BAR_REF].setAnimationValue(currentValue);
       },
     });
 
@@ -57,15 +58,16 @@ var ScrollableTabView = React.createClass({
       onPanResponderRelease: (e, gestureState) => {
         var relativeGestureDistance = gestureState.dx / deviceWidth,
             lastPageIndex = this.props.children.length - 1,
-            vx = gestureState.vx;
+            vx = gestureState.vx,
+            newPage = this.state.currentPage;
 
-        if (this._currentPage != lastPageIndex && (relativeGestureDistance < -0.5 || (relativeGestureDistance < 0 && vx <= 0.5))) {
-          this._currentPage = this._currentPage + 1;
-        } else if (this._currentPage != 0 && (relativeGestureDistance > 0.5 || (relativeGestureDistance > 0 && vx >= 0.5))) {
-          this._currentPage = this._currentPage - 1;
+        if (this.state.currentPage !== lastPageIndex && (relativeGestureDistance < -0.5 || (relativeGestureDistance < 0 && vx <= 0.5))) {
+          newPage = newPage + 1;
+        } else if (this.state.currentPage !== 0 && (relativeGestureDistance > 0.5 || (relativeGestureDistance > 0 && vx >= 0.5))) {
+          newPage = newPage - 1;
         }
 
-        this._scrollSpring.setEndValue(this._currentPage);
+        this.goToPage(newPage);
       },
 
       // Dragging, move the view with the touch
@@ -73,14 +75,14 @@ var ScrollableTabView = React.createClass({
         var dx = gestureState.dx;
         var lastPageIndex = this.props.children.length - 1;
 
-        if (this._currentPage == 0 && dx > 0) {
+        if (this.state.currentPage === 0 && dx > 0) {
           // Don't set the spring if we're on the first page and trying to move before it
-        } else if (this._currentPage == lastPageIndex && dx < 0) {
+        } else if (this.state.currentPage === lastPageIndex && dx < 0) {
           // Don't set the spring if we're already on the last page and trying to move to the next
         } else {
           // This is awkward because when we are scrolling we are offsetting the underlying view
           // to the left (-x)
-          var offsetX = dx - (this._currentPage * deviceWidth);
+          var offsetX = dx - (this.state.currentPage * deviceWidth);
           this._scrollSpring.setCurrentValue(-1 * offsetX / deviceWidth);
         }
       },
@@ -100,74 +102,42 @@ var ScrollableTabView = React.createClass({
   },
 
   goToPage(pageNumber) {
-    this._scrollSpring.setEndValue(pageNumber);
-    this._currentPage = pageNumber;
-    this.props.onChangeTab &&
+    this.props.currentPage !== pageNumber && this.props.onChangeTab &&
       this.props.onChangeTab({i: pageNumber, ref: this.props.children[pageNumber]});
+
+    this._scrollSpring.setEndValue(pageNumber);
+    this.setState({currentPage: pageNumber});
   },
 
-  renderTabOption(name, page) {
-    return (
-      <TouchableOpacity key={name} onPress={() => this.goToPage(page)}>
-         <View style={styles.tab}>
-          <Text>{name}</Text>
-         </View>
-      </TouchableOpacity>
-    );
+  renderTabBar(props) {
+    if (this.props.renderTabBar) {
+      return React.cloneElement(this.props.renderTabBar(), props);
+    } else {
+      return <DefaultTabBar {...props} />;
+    }
   },
 
   render() {
-    var numberOfTabs = this.props.children.length;
-    var tabUnderlineOptions = this.props.tabUnderlineOptions || {};
-    var tabUnderlineStyle = {
-      position: 'absolute',
-      width: deviceWidth / numberOfTabs,
-      height: tabUnderlineOptions.height || 4,
-      backgroundColor: tabUnderlineOptions || 'navy',
-      bottom: 0,
-    }
-
     var sceneContainerStyle = {
       width: deviceWidth * this.props.children.length,
       flex: 1,
       flexDirection: 'row'
-    }
+    };
 
     return (
       <View style={{flex: 1}}>
-        <View style={styles.tabs}>
-          {this.props.children.map((child, i) => this.renderTabOption(child.props.tabLabel, i))}
-          <View style={tabUnderlineStyle}
-                ref={view => { this.tabUnderline = view; }} />
-        </View>
+        {this.renderTabBar({goToPage: this.goToPage.bind(this),
+                            tabs: this.props.children.map((child) => child.props.tabLabel),
+                            activeTab: this.state.currentPage,
+                            ref: TAB_BAR_REF})}
 
         <View style={sceneContainerStyle} {...this._panResponder.panHandlers}
               ref={view => { this.scrollView = view; }}>
           {this.props.children}
         </View>
       </View>
-    )
+    );
   }
-});
-
-var styles = StyleSheet.create({
-  tab: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingBottom: 10,
-  },
-
-  tabs: {
-    height: 50,
-    flexDirection: 'row',
-    marginTop: 20,
-    borderWidth: 1,
-    borderTopWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderBottomColor: '#ccc',
-  },
 });
 
 module.exports = ScrollableTabView;
