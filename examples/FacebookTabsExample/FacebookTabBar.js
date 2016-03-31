5358fb7a63f793eb1f35573ce53b37899acc7ba8
Sokovikov <skv-headless@yandex.ru>
2016-03-31 09:59:51 +0100

    fix eslint warnings in example, migrate to es6 imports

--- examples/FacebookTabsExample/FacebookTabBar.js
+++ examples/FacebookTabsExample/FacebookTabBar.js
@@ -1,17 +1,83 @@
-'use strict';
-
-var React = require('react-native');
-var {
+import React, {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
   Animated,
-} = React;
+} from 'react-native';
 
-const Icon = require('react-native-vector-icons/Ionicons');
+import Icon from 'react-native-vector-icons/Ionicons';
 
-var styles = StyleSheet.create({
+const FacebookTabBar = React.createClass({
+  selectedTabIcons: [],
+  unselectedTabIcons: [],
+
+  propTypes: {
+    goToPage: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    tabs: React.PropTypes.array,
+  },
+
+  renderTabOption(name, page) {
+    const isTabActive = this.props.activeTab === page;
+
+    return (
+      <TouchableOpacity key={name} onPress={() => this.props.goToPage(page)} style={styles.tab}>
+        <Icon name={name} size={30} color='#3B5998' style={styles.icon}
+              ref={(icon) => { this.selectedTabIcons[page] = icon; }}/>
+        <Icon name={name} size={30} color='#ccc' style={styles.icon}
+              ref={(icon) => { this.unselectedTabIcons[page] = icon; }}/>
+      </TouchableOpacity>
+    );
+  },
+
+  componentDidMount() {
+    this.setAnimationValue({ value: this.props.activeTab, });
+    this._listener = this.props.scrollValue.addListener(this.setAnimationValue);
+  },
+
+  setAnimationValue({ value, }) {
+    this.unselectedTabIcons.forEach((icon, i) => {
+      let iconRef = icon;
+
+      if (!icon.setNativeProps && icon !== null) {
+        iconRef = icon.refs.icon_image;
+      }
+
+      if (value - i >= 0 && value - i <= 1) {
+        iconRef.setNativeProps({ style: { opacity: value - i, }, });
+      }
+      if (i - value >= 0 &&  i - value <= 1) {
+        iconRef.setNativeProps({ style: { opacity: i - value, }, });
+      }
+    });
+  },
+
+  render() {
+    const containerWidth = this.props.containerWidth;
+    const numberOfTabs = this.props.tabs.length;
+    const tabUnderlineStyle = {
+      position: 'absolute',
+      width: containerWidth / numberOfTabs,
+      height: 3,
+      backgroundColor: '#3b5998',
+      bottom: 0,
+    };
+
+    const left = this.props.scrollValue.interpolate({
+      inputRange: [0, 1, ], outputRange: [0, containerWidth / numberOfTabs, ],
+    });
+
+    return <View>
+      <View style={[styles.tabs, this.props.style, ]}>
+        {this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
+      </View>
+      <Animated.View style={[tabUnderlineStyle, { left, }, ]} />
+    </View>;
+  },
+});
+
+const styles = StyleSheet.create({
   tab: {
     flex: 1,
     alignItems: 'center',
@@ -35,77 +101,4 @@ var styles = StyleSheet.create({
   },
 });
 
-var FacebookTabBar = React.createClass({
-  selectedTabIcons: [],
-  unselectedTabIcons: [],
-
-  propTypes: {
-    goToPage: React.PropTypes.func,
-    activeTab: React.PropTypes.number,
-    tabs: React.PropTypes.array
-  },
-
-  renderTabOption(name, page) {
-    var isTabActive = this.props.activeTab === page;
-
-    return (
-      <TouchableOpacity key={name} onPress={() => this.props.goToPage(page)} style={styles.tab}>
-        <Icon name={name} size={30} color='#3B5998' style={styles.icon}
-              ref={(icon) => { this.selectedTabIcons[page] = icon }}/>
-        <Icon name={name} size={30} color='#ccc' style={styles.icon}
-              ref={(icon) => { this.unselectedTabIcons[page] = icon }}/>
-      </TouchableOpacity>
-    );
-  },
-
-  componentDidMount() {
-    this.setAnimationValue({value: this.props.activeTab});
-    this._listener = this.props.scrollValue.addListener(this.setAnimationValue);
-  },
-
-  setAnimationValue({value}) {
-    var currentPage = this.props.activeTab;
-
-    this.unselectedTabIcons.forEach((icon, i) => {
-      var iconRef = icon;
-
-      if (!icon.setNativeProps && icon !== null) {
-        iconRef = icon.refs.icon_image
-      }
-
-      if (value - i >= 0 && value - i <= 1) {
-        iconRef.setNativeProps({ style: {opacity: value - i} });
-      }
-      if (i - value >= 0 &&  i - value <= 1) {
-        iconRef.setNativeProps({ style: {opacity: i - value} });
-      }
-    });
-  },
-
-  render() {
-    var containerWidth = this.props.containerWidth;
-    var numberOfTabs = this.props.tabs.length;
-    var tabUnderlineStyle = {
-      position: 'absolute',
-      width: containerWidth / numberOfTabs,
-      height: 3,
-      backgroundColor: '#3b5998',
-      bottom: 0,
-    };
-
-    var left = this.props.scrollValue.interpolate({
-      inputRange: [0, 1], outputRange: [0, containerWidth / numberOfTabs]
-    });
-
-    return (
-      <View>
-        <View style={[styles.tabs, this.props.style, ]}>
-          {this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
-        </View>
-        <Animated.View style={[tabUnderlineStyle, {left}]} />
-      </View>
-    );
-  },
-});
-
-module.exports = FacebookTabBar;
+export default FacebookTabBar;
