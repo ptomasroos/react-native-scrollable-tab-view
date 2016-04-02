51c6ff9878143431ccdf9d773f8138437e89d9e2
Sokovikov <skv-headless@yandex.ru>
2016-04-02 21:36:45 +0100

    esling fixes in DefaultTabBar

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -1,22 +1,74 @@
-'use strict';
-
-var React = require('react-native');
-var {
+const React = require('react-native');
+const {
   StyleSheet,
   Text,
   View,
   Animated,
 } = React;
-var Button = require('./Button');
+const Button = require('./Button');
 
-var styles = StyleSheet.create({
+const DefaultTabBar = React.createClass({
+  propTypes: {
+    goToPage: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    tabs: React.PropTypes.array,
+    underlineColor: React.PropTypes.string,
+    backgroundColor: React.PropTypes.string,
+    activeTextColor: React.PropTypes.string,
+    inactiveTextColor: React.PropTypes.string,
+  },
+
+  renderTabOption(name, page) {
+    const isTabActive = this.props.activeTab === page;
+    const activeTextColor = this.props.activeTextColor || 'navy';
+    const inactiveTextColor = this.props.inactiveTextColor || 'black';
+
+    return <Button
+      key={name}
+      accessible={true}
+      accessibilityLabel={name}
+      accessibilityTraits='button'
+      onPress={() => this.props.goToPage(page)}
+    >
+      <View style={styles.tab}>
+        <Text style={{color: isTabActive ? activeTextColor : inactiveTextColor, fontWeight: isTabActive ? 'bold' : 'normal', }}>
+          {name}
+        </Text>
+      </View>
+    </Button>;
+  },
+
+  render() {
+    const containerWidth = this.props.containerWidth;
+    const numberOfTabs = this.props.tabs.length;
+    const tabUnderlineStyle = {
+      position: 'absolute',
+      width: containerWidth / numberOfTabs,
+      height: 4,
+      backgroundColor: this.props.underlineColor || 'navy',
+      bottom: 0,
+    };
+
+    const left = this.props.scrollValue.interpolate({
+      inputRange: [0, 1, ], outputRange: [0,  containerWidth / numberOfTabs, ],
+    });
+
+    return (
+      <View style={[styles.tabs, {backgroundColor: this.props.backgroundColor || null, }, this.props.style, ]}>
+        {this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
+        <Animated.View style={[tabUnderlineStyle, { left, }, ]} />
+      </View>
+    );
+  },
+});
+
+const styles = StyleSheet.create({
   tab: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     paddingBottom: 10,
   },
-
   tabs: {
     height: 50,
     flexDirection: 'row',
@@ -26,61 +78,6 @@ var styles = StyleSheet.create({
     borderLeftWidth: 0,
     borderRightWidth: 0,
     borderBottomColor: '#ccc',
-  },
-});
-
-var DefaultTabBar = React.createClass({
-  propTypes: {
-    goToPage: React.PropTypes.func,
-    activeTab: React.PropTypes.number,
-    tabs: React.PropTypes.array,
-    underlineColor : React.PropTypes.string,
-    backgroundColor : React.PropTypes.string,
-    activeTextColor : React.PropTypes.string,
-    inactiveTextColor : React.PropTypes.string,
-  },
-
-  renderTabOption(name, page) {
-    var isTabActive = this.props.activeTab === page;
-    var activeTextColor = this.props.activeTextColor || "navy";
-    var inactiveTextColor = this.props.inactiveTextColor || "black";
-
-    return <Button
-      key={name}
-      accessible={true}
-      accessibilityLabel={name}
-      accessibilityTraits='button'
-      onPress={() => this.props.goToPage(page)}
-    >
-      <View style={[styles.tab]}>
-        <Text style={{color: isTabActive ? activeTextColor : inactiveTextColor, fontWeight: isTabActive ? 'bold' : 'normal'}}>
-          {name}
-        </Text>
-      </View>
-    </Button>;
-  },
-
-  render() {
-    var containerWidth = this.props.containerWidth;
-    var numberOfTabs = this.props.tabs.length;
-    var tabUnderlineStyle = {
-      position: 'absolute',
-      width: containerWidth / numberOfTabs,
-      height: 4,
-      backgroundColor: this.props.underlineColor || "navy",
-      bottom: 0,
-    };
-
-    var left = this.props.scrollValue.interpolate({
-      inputRange: [0, 1], outputRange: [0,  containerWidth / numberOfTabs]
-    });
-
-    return (
-      <View style={[styles.tabs, {backgroundColor : this.props.backgroundColor || null}, this.props.style, ]}>
-        {this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
-        <Animated.View style={[tabUnderlineStyle, {left}]} />
-      </View>
-    );
   },
 });
 
