deef2439ee4ff6fafd868f22aaccd2009e159e6a
Sokovikov <skv-headless@yandex.ru>
2016-05-27 12:22:37 +0300

    explicit usage of DefaultTabBar in example

--- examples/FacebookTabsExample/SimpleExample.js
+++ examples/FacebookTabsExample/SimpleExample.js
@@ -3,11 +3,14 @@ import {
   Text,
 } from 'react-native';
 
-import ScrollableTabView from 'react-native-scrollable-tab-view';
+import ScrollableTabView, {DefaultTabBar} from 'react-native-scrollable-tab-view';
 
 export default React.createClass({
   render() {
-    return <ScrollableTabView style={{marginTop: 20, }}>
+    return <ScrollableTabView
+      style={{marginTop: 20, }}
+      renderTabBar={() => <DefaultTabBar />}
+    >
       <Text tabLabel='Tab #1'>My</Text>
       <Text tabLabel='Tab #2'>favorite</Text>
       <Text tabLabel='Tab #3'>project</Text>
