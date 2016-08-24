6ccc1fa050225555dc93274f236319e8eed87cb8
reyesr <reyes.rr@gmail.com>
2016-08-24 15:00:01 +0200

    fix crash on android <= 4.4

--- Button.android.js
+++ Button.android.js
@@ -2,13 +2,15 @@ const React = require('react');
 const ReactNative = require('react-native');
 const {
   TouchableNativeFeedback,
-  View,
+  View, Platform
 } = ReactNative;
+
+const API21 = (Platform.OS === "android" && Platform["Version"] >= 21);
 
 const Button = (props) => {
   return <TouchableNativeFeedback
     delayPressIn={0}
-    background={TouchableNativeFeedback.SelectableBackgroundBorderless()} // eslint-disable-line new-cap
+    background={API21 ? TouchableNativeFeedback.SelectableBackgroundBorderless() : TouchableNativeFeedback.SelectableBackground()} // eslint-disable-line new-cap
     {...props}
   >
     {props.children}
