cfd2cd09de2504df5df3cbc3ac199e6995d41552
Bruno Barbieri <brunobar79@gmail.com>
2016-09-02 14:13:53 -0400

    round width when using onLayout

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ const ScrollableTabView = React.createClass({
   _handleLayout(e) {
     const { width, } = e.nativeEvent.layout;
 
-    if (width !== this.state.containerWidth) {
+    if (Math.round(width, 2) !== Math.round(this.state.containerWidth, 2)) {
       this.setState({ containerWidth: width, });
       this.requestAnimationFrame(() => {
         this.goToPage(this.state.currentPage);
