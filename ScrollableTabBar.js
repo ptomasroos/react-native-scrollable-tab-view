27ca1d732fcae659c4b9c8758fefb6f9539d7f2d
Sokovikov <skv-headless@yandex.ru>
2016-09-26 10:38:07 +0400

    use !== instead of !=

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -192,7 +192,7 @@ const ScrollableTabBar = React.createClass({
 
   componentWillReceiveProps(nextProps) {
     // If the tabs change, force the width of the tabs container to be recalculated
-    if (JSON.stringify(this.props.tabs) != JSON.stringify(nextProps.tabs) && this.state._containerWidth) {
+    if (JSON.stringify(this.props.tabs) !== JSON.stringify(nextProps.tabs) && this.state._containerWidth) {
       this.setState({ _containerWidth: null, });
     }
   },
