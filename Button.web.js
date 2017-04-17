88fc2e4571c78d66c873d90851a4a4f15ff92007
Amogh Banta <amoghbanta@gmail.com>
2017-04-17 12:52:06 +0530

    Update Button for web

--- Button.web.js
+++ Button.web.js
@@ -1,1 +1,15 @@
-B
+
+const React = require('react');
+const ReactNative = require('react-native');
+const {
+ TouchableOpacity,
+  View,
+} = ReactNative;
+
+const Button = (props) => {
+  return <TouchableOpacity {...props}>
+    {props.children}
+  </TouchableOpacity>;
+};
+
+module.exports = Button;
