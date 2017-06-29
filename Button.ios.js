e379373d2155b442cf9f61d944004991b3d54b8e
Daniel Carns <carns.dan.a@gmail.com>
2017-06-29 09:57:29 -0400

    Update Button.ios.js
    View was unused and its a multi-line return so it should have enclosing parenthesis.

--- Button.ios.js
+++ Button.ios.js
@@ -2,13 +2,14 @@ const React = require('react');
 const ReactNative = require('react-native');
 const {
  TouchableOpacity,
-  View,
 } = ReactNative;
 
 const Button = (props) => {
-  return <TouchableOpacity {...props}>
+  return (
+   <TouchableOpacity {...props}>
     {props.children}
-  </TouchableOpacity>;
+   </TouchableOpacity>
+  );
 };
 
 module.exports = Button;
