76013d0b999070815c75378fb8b632fbf8d91001
Sondre Mære Overskaug <sondremo@gmail.com>
2017-10-06 14:22:32 +0200

    feat: add support for icons
    BREAKING CHANGE

---
+++ Tab.js
@@ -0,0 +1,14 @@
+const React = require('react');
+
+const Tab = props => props.children;
+
+Tab.propTypes = {
+  children: React.PropTypes.element,
+  tabLabel: React.PropTypes.string,
+  icon: React.PropTypes.element,
+  activeIcon: React.PropTypes.element,
+};
+
+Tab.displayName = 'Tab';
+
+module.exports = Tab;
