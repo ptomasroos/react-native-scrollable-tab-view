3df42397b11fdcd0e3d4b3cdb96ed1f3b65ca3a1
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-12-27 21:03:47 +0300

    ReactNative is not defined error fixed.

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -1,5 +1,6 @@
 const React = require('react');
-const { ViewPropTypes } = ReactNative = require('react-native');
+const ReactNative = require('react-native');
+const { ViewPropTypes } = ReactNative;
 const PropTypes = require('prop-types');
 const createReactClass = require('create-react-class');
 const {
