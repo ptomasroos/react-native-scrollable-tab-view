ab14f348506380bbe833e93e39af1db6c19a01e8
Tomas Roos <ptomasroos@gmail.com>
2018-10-25 11:23:47 +0200

    Updated to ES6

--- Button.ios.js
+++ Button.ios.js
@@ -1,14 +1,7 @@
-const React = require('react');
-const ReactNative = require('react-native');
-const {
- TouchableOpacity,
-  View,
-} = ReactNative;
+import { TouchableOpacity } from "react-native";
 
-const Button = (props) => {
-  return <TouchableOpacity {...props}>
-    {props.children}
-  </TouchableOpacity>;
+const Button = props => {
+  return <TouchableOpacity {...props}>{props.children}</TouchableOpacity>;
 };
 
-module.exports = Button;
+export default Button;
