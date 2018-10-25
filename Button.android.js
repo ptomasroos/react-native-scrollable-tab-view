3c948f72bdb81d7b95e33b84116b50477b2677a3
Tomas Roos <ptomasroos@gmail.com>
2018-10-25 11:22:43 +0200

    Updated to ES6

--- Button.android.js
+++ Button.android.js
@@ -1,18 +1,16 @@
-const React = require('react');
-const ReactNative = require('react-native');
-const {
-  TouchableNativeFeedback,
-  View,
-} = ReactNative;
+import React from "react";
+import { TouchableNativeFeedback } from "react";
 
-const Button = (props) => {
-  return <TouchableNativeFeedback
-    delayPressIn={0}
-    background={TouchableNativeFeedback.SelectableBackground()} // eslint-disable-line new-cap
-    {...props}
-  >
-    {props.children}
-  </TouchableNativeFeedback>;
+const Button = props => {
+  return (
+    <TouchableNativeFeedback
+      delayPressIn={0}
+      background={TouchableNativeFeedback.SelectableBackground()}
+      {...props}
+    >
+      {props.children}
+    </TouchableNativeFeedback>
+  );
 };
 
-module.exports = Button;
+export default Button;
