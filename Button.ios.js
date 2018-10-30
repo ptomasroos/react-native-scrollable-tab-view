d7af0142fee14d60cbdd5904f69a3eb635b40149
Tomas Roos <ptomasroos@gmail.com>
2018-10-30 12:06:46 +0100

    Updated Button.ios and flow support

--- Button.ios.js
+++ Button.ios.js
@@ -1,7 +1,16 @@
+import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
-const Button = props => {
-  return <TouchableOpacity {...props}>{props.children}</TouchableOpacity>;
+type Props = typeof TouchableOpacity.props & {
+  children?: React.Node,
 };
+
+class Button extends React.Component<Props> {
+  render() {
+    const { children, ...props } = this.props;
+
+    return <TouchableOpacity {...props}>{children}</TouchableOpacity>;
+  }
+}
 
 export default Button;
