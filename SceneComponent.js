47ea82384e4b054c41e5635edfdd184365c2022d
Tomas Roos <ptomasroos@gmail.com>
2018-10-30 12:08:35 +0100

    Updated SceneComponent with flow support instead of prop-types

--- SceneComponent.js
+++ SceneComponent.js
@@ -1,15 +1,23 @@
+// @flow
+import React from 'react';
 import { View } from 'react-native';
+
 import StaticContainer from './StaticContainer';
 
-const SceneComponent = Props => {
-  const { shouldUpdated, ...props } = Props;
-  return (
-    <View {...props}>
-      <StaticContainer shouldUpdate={shouldUpdated}>
-        {props.children}
-      </StaticContainer>
-    </View>
-  );
-};
+type Props = typeof View.props & typeof StaticContainer.props;
+
+class SceneComponent extends React.Component<Props> {
+  render() {
+    const { shouldUpdated, children, ...props } = this.props;
+
+    return (
+      <View {...props}>
+        <StaticContainer shouldUpdate={shouldUpdated}>
+          {children}
+        </StaticContainer>
+      </View>
+    );
+  }
+}
 
 export default SceneComponent;
