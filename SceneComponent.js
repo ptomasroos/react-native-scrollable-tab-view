aee6afff7bdb70a00316747e8dbcf1668af04a1d
Jean-Frédéric Durand <jfdurk@gmx.fr>
2019-06-07 09:14:59 -0400

    linter fix

--- SceneComponent.js
+++ SceneComponent.js
@@ -1,12 +1,12 @@
 const React = require('react');
 const ReactNative = require('react-native');
-const {Component } = React;
-const {View, StyleSheet } = ReactNative;
+const { Component } = React;
+const { View, StyleSheet } = ReactNative;
 
 const StaticContainer = require('./StaticContainer');
 
 const SceneComponent = (Props) => {
-  const {shouldUpdated, ...props} = Props;
+  const { shouldUpdated, ...props } = Props;
   return <View {...props}>
       <StaticContainer shouldUpdate={shouldUpdated}>
         {props.children}
