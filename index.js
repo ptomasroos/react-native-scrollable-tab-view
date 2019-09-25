404bf4a11055675cb7b6a99bdc60c7e827985a3f
diegolmello <diegolmello@gmail.com>
2019-09-25 15:53:30 -0300

    remove deprecated ViewPager

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const {
   ScrollView,
   Platform,
   StyleSheet,
-  ViewPagerAndroid,
   InteractionManager,
 } = ReactNative;
 const TimerMixin = require('react-timer-mixin');
+import ViewPager from "@react-native-community/viewpager";
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
 const ScrollableTabBar = require('./ScrollableTabBar');
 
 const AnimatedViewPagerAndroid = Platform.OS === 'android' ?
-  Animated.createAnimatedComponent(ViewPagerAndroid) :
+  Animated.createAnimatedComponent(ViewPager) :
   undefined;
 
 const ScrollableTabView = createReactClass({
