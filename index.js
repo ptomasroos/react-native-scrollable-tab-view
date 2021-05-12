f1202adcbf643c79e6c17c453d425ec70dfb2d41
Rishu Agrawal <rishuagarwal@microsoft.com>
2021-05-13 00:49:25 +0530

    Handling infinite rerendering of tab view when using page props with onChangeTab

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ const ScrollableTabView = createReactClass({
       this.updateSceneKeys({ page: this.state.currentPage, children: this.props.children, });
     }
 
-    if (this.props.page >= 0 && this.props.page !== this.state.currentPage) {
+    if (this.props.page !== prevProps.page && this.props.page >= 0 && this.props.page !== this.state.currentPage) {
       this.goToPage(this.props.page);
     }
   },
