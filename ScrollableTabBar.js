f8e0bb826378ee222c6d967db6b05f39d6b209cc
shrutishankar28 <shrutishankar28@gmail.com>
2021-07-29 11:09:40 +0530

    Modified variable declarations

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const { ViewPropTypes } = ReactNative = require('react-native');
+const ReactNative = require('react-native');
 const PropTypes = require('prop-types');
 const createReactClass = require('create-react-class');
 const {
@@ -10,6 +10,7 @@ const {
   Text,
   Platform,
   Dimensions,
+  ViewPropTypes
 } = ReactNative;
 const Button = require('./Button');
 
